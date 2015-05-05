fa57b25dcfbef82ae5fec587c114f1fbc25cf2d5
paul_cleary@mckinsey.com <pauljohncleary@gmail.com>
2015-05-05 14:17:54 +0100

    tests, updates etc.

--- index.js
+++ index.js
@@ -1,52 +1,27 @@
 #!/usr/bin/env node
 
 var request = require('request'),
-  //cheerio = require('cheerio'),
-  url = require('url'),
-  validUrl = require('valid-url');
+  cheerio = require('cheerio'),
+  linkIsValid = require('./lib/validLink');
 
-var statusObj = {};
-
-function inValidateStatus(message) {
-  statusObj.status = false;
-  statusObj.message = message;
-  return statusObj;
-}
-
-function checkValidity(link, callback) {
-'use strict';
-  try { //this can throw an error if the link isn't a string
-    var parsedUrl = url.parse(link);
-    if( parsedUrl.href.substring(0,5) === 'javas' || parsedUrl.href.substring(0,6) === 'mailto') {
-      inValidateStatus('Not a web resource');
-    } else if (!validUrl.isWebUri(link)) {
-      inValidateStatus('Malformed');
-    }
+function start (url, cb) {
+  if(linkIsValid(url)) {
+    request(url, function (error, response, body) {
+      if(error) {
+        return cb({
+          error: error
+        });
+      } else {
+        var $ = cheerio.load(body);
+        return cb({
+          isWebResource: true,
+          title: $('title').text()
+        });
+      }
+    });
+  } else {
+    return cb({isWebResource: false});
   }
-  catch(e) {
-    inValidateStatus('Malformed');
-  }
-  return callback();
 }
-
-function start (url) {
-  return checkValidity(url, function() {
-    if(statusObj === false) {
-      return statusObj;
-    } else {
-      request(url, function (error, response, body) {
-        if(error) {
-          return inValidateStatus(response.statusCode);
-        }
-        if (!error && response.statusCode === 200) {
-          console.log(body);
-        }
-      });
-      //if ok, send a request
-      //parse request response using cheerio for the title, favicon, image etc.
-    }
-  });
-}
-
 
 module.exports = start;
